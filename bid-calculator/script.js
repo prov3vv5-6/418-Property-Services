20f5e07ff63ce3bd786907a18622358d838f454c
Justin <111033269+prov3vv5-6@users.noreply.github.com>
2026-06-30 18:26:42 -0600

    add calculateBid() function with logic

---
+++ bid-calculator/script.js
@@ -0,0 +1,55 @@
+const calcBtn = document.getElementById("calcBtn");
+const equipmentEl = document.getElementById("equipment");
+const fullnessEl = document.getElementById("fullness");
+const dumpWeightEl = document.getElementById("dumpWeight");
+const laborWorkersEl = document.getElementById("laborWorkers");
+const laborHoursEl = document.getElementById("laborHours");
+const travelMilesEl = document.getElementById("travelMiles");
+const resultEl = document.getElementById("result");
+
+// RATES
+const PRICE_PER_YARD = 40; // $ per cubic yard
+const DUMP_RATE = 60; // $ per ton of heavy debris
+const LABOR_RATE = 35; // $ per worker per hour
+const FREE_MILES = 15; // free travel radius
+const PER_MILE = 2; // $ per mile beyond the free radius
+const MIN_CHARGE = 95; // minimum bid
+
+calcBtn.addEventListener("click", calculateBid);
+
+// Calculate Bid Function
+function calculateBid() {
+  // Convert from string to decimal
+  const capacity = parseFloat(equipmentEl.value);
+  const fullness = parseFloat(fullnessEl.value);
+  const dumpWeight = parseFloat(dumpWeightEl.value);
+  const laborWorkers = parseFloat(laborWorkersEl.value);
+  const laborHours = parseFloat(laborHoursEl.value);
+  const travelMiles = parseFloat(travelMilesEl.value);
+
+  // Calculate base price
+  const volume = capacity * fullness;
+  const basePrice = volume * PRICE_PER_YARD;
+
+  console.log(`Volume: ${volume} BasePrice: $${basePrice}`);
+
+  // Calculate dump fee
+  const dumpFee = dumpWeight * DUMP_RATE;
+
+  // Calculate labor cost
+  const laborCost = laborWorkers * laborHours * LABOR_RATE;
+
+  // Calculate Travel
+  const billableMiles = Math.max(0, travelMiles - FREE_MILES);
+  const travelCost = billableMiles * PER_MILE;
+
+  // Subtotal
+  const subtotal = basePrice + dumpFee + laborCost + travelCost;
+  console.log(`Subtotal: $${subtotal}`);
+
+  // Check to make sure minimum charge is selected if subtotal is lower than MIN_CHARGE, then store in total
+  const total = Math.max(subtotal, MIN_CHARGE);
+
+  // Display the total
+  resultEl.textContent = `$${total.toFixed(2)}`;
+}
